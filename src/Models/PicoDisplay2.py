bb77689e9dbbdc498149e0c6bfdca1f393107c9e
Raúl Caro Pastorino <raul@fryntiz.dev>
2024-10-11 18:07:26 +0200

    Model base for Display

---
+++ src/Models/PicoDisplay2.py
@@ -0,0 +1,20 @@
+class PicoDisplay2:
+
+    def __init__(self, controller, debug=False) -> None:
+        self.DEBUG = debug
+        self.controller = controller
+
+        self.clear()
+
+        self.update()
+
+    # a handy function we can call to clear the screen
+    # display.set_pen(15) is white and display.set_pen(0) is black
+    def clear(self):
+        pass
+
+    def update(self):
+        """
+        Actualiza la información de la pantalla
+        """
+        pass
